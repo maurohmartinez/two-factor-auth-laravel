e0ee3213c8fe99fe8adbdce5c59b95d8929bb4de
maurohmartinez <maurohmartinez@gmail.com>
2022-05-07 23:44:27 +0300

    Forms views improved part 2

--- src/config/two_factor_auth.php
+++ src/config/two_factor_auth.php
@@ -15,7 +15,7 @@ return [
     'texts' => [
         'setup_title' => 'Setup Google Authenticator',
         'setup_description' => 'Configure your two-factor authentication by scanning the following code. Alternatively, enter it manually:',
-        'validate_title' => 'Validate Google Authenticator',
+        'validate_title' => 'Validate with Google Authenticator',
         'validate_description' => 'Click the following link to reset your Google Authenticator App:',
         're_setup_btn' => 'Reset',
     ],
--- src/resources/views/form.blade.php
+++ src/resources/views/form.blade.php
@@ -1,10 +1,10 @@
 <section class="bg-light" style="height: 100vh;">
     <div class="container py-5 h-100">
-        <div class="row d-flex justify-content-center align-items-center h-100">
+        <div class="row d-md-flex justify-content-center align-items-center h-100">
             <div class="col-xl-10">
-                <div class="card rounded-3 text-black">
+                <div class="card rounded-3">
                     <div class="row g-0">
-                        <div class="col-lg-6">
+                        <div class="col-12 col-md-6 order-2 order-md-1">
                             <div class="card-body p-md-5 mx-md-4">
                                 <div class="text-center">
                                     <h4 class="mt-1 mb-3 pb-1">{{ $formTitle }}</h4>
@@ -34,11 +34,15 @@
                                     </div>
 
                                 </form>
-
                             </div>
                         </div>
-                        <div class="col-lg-6 d-flex align-items-center gradient-custom-2">
-                            <img src="{{  }}" alt="QR">
+                        <div class="col-md-6 order-md-2 d-none d-md-block" style="background-position: center; background-size: cover; background-image: url('{{ asset('vendor/two_factor_auth/google-authenticator.jpg') }}');"></div>
+                        <div class="col-12 order-1 d-block d-md-none">
+                            <div style="background-position: center; background-size: cover; background-image: url('{{ asset('vendor/two_factor_auth/google-authenticator.jpg') }}');">
+                                <div class="d-block d-md-none p-5"></div>
+                                <div class="d-block d-md-none p-5"></div>
+                                <div class="d-block d-md-none p-5"></div>
+                            </div>
                         </div>
                     </div>
                 </div>
