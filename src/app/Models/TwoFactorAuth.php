9c8d0c991504db5f50f5cf3d0dfe320b318261be
maurohmartinez <maurohmartinez@gmail.com>
2022-05-08 15:49:55 +0300

    Several fixes - Readme updated 2

--- src/app/Models/TwoFactorAuth.php
+++ src/app/Models/TwoFactorAuth.php
@@ -16,11 +16,6 @@ class TwoFactorAuth extends Model
     protected $table = 'two_factor_auth';
     protected $fillable = ['user_id', 'secret'];
 
-    public function getSecretAttribute(string $value): ?string
-    {
-        return $this->attributes['secret'] = $value ? decrypt($value) : null;
-    }
-
     public function setSecretAttribute(string $value): void
     {
         $this->attributes['secret'] = encrypt($value);
