486394f45c5626625e834cabc69faf904f5e9edb
maurohmartinez <maurohmartinez@gmail.com>
2022-05-10 10:10:06 +0300

    Views updated

--- src/resources/lang/en/form.php
+++ src/resources/lang/en/form.php
@@ -2,12 +2,8 @@
 
 return [
     'setup_title' => 'Set Up Two-Factor Authentication',
-    'setup_description_1' => 'This site requires two-factor authentication to access this site. 
-        Two-factor authentication is a combination of something you know (your password) and something you have (such as a smartphone app). 
-        You can choose Google Authenticator, LassPass Authenticator, One Password, or Microsoft Authenticator. 
-        These apps are free to download to your mobile phone.',
-    'setup_description_2' => 'Configure your two-factor authentication by scanning the following code. 
-        You can also manually enter the following code into your app:',
+    'warning' => 'This site requires two-factor authentication to access this site. You can choose Google Authenticator, LassPass Authenticator, One Password, or Microsoft Authenticator. ',
+    'description' => 'Configure your two-factor authentication by scanning the QR code. You can also manually enter the following code into your app:',
     'validate_title' => 'Validate with Two-Factor Authentication',
     'validate_description' => 'Click the following link to reset your Two-Factor Authentication App:',
     're_setup_btn' => 'Reset',
--- src/resources/lang/es/form.php
+++ src/resources/lang/es/form.php
@@ -2,11 +2,8 @@
 
 return [
     'setup_title' => 'Configurar la autenticación de dos factores',
-    'setup_description' => 'Este sitio requiere una autenticación de dos factores para acceder a él. 
-        La autenticación de dos factores es una combinación de algo que usted conoce (su contraseña) y algo que tiene (como una aplicación para teléfonos). 
-        Puede elegir Google Authenticator, LassPass Authenticator, One Password o Microsoft Authenticator. 
-        Estas aplicaciones se pueden descargar gratuitamente en tu teléfono móvil. Configure su autenticación de dos factores escaneando el siguiente código. 
-        También puede introducir manualmente el siguiente código en su aplicación:',
+    'warning' => 'Este sitio requiere una autenticación de dos factores para acceder a él. Puede elegir Google Authenticator, LassPass Authenticator, One Password o Microsoft Authenticator.',
+    'description' => 'Configure su autenticación de dos factores escaneando el código QR. También puedes introducir manualmente el siguiente código en tu aplicación:',
     'validate_title' => 'Validación con autenticación de dos factores',
     'validate_description' => 'Haga clic en el siguiente enlace para restablecer su aplicación de autenticación de dos factores:',
     're_setup_btn' => 'Reconfigurar',
